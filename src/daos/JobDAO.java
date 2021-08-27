f4c8d109a9b0ba97e2941685963970285c9b7508
Kelvin Kurniawan Oktavianto <37462966+kelvinkurniawan@users.noreply.github.com>
2021-08-27 10:27:20 +0700

    Add job model, getAll method at JobDAO

---
+++ src/daos/JobDAO.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daos;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import models.Job;
+import tools.Query;
+
+/**
+ *
+ * @author kelvi
+ */
+public class JobDAO implements DAOInterface<Job, String>{
+    
+    private final Connection connection;
+    
+    public JobDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    @Override
+    public List<Job> getAll() {
+        List<Job> jobs = new ArrayList<>();
+        
+        try {
+            ResultSet resultSet = connection
+                    .prepareStatement(Query.GET_REGION.getDisplayQuery())
+                    .executeQuery();    
+            
+            System.out.println(resultSet);
+            
+            while(resultSet.next()) {
+                Job job = new Job();
+                jobs.add(new Job(resultSet.getString(1), resultSet.getString(2), resultSet.getDouble(3), resultSet.getDouble(4)));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return jobs;
+    }
+
+    @Override
+    public Object getById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean save(Job object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/tools/Query.java
+++ src/tools/Query.java
@@ -11,12 +11,20 @@ package tools;
  */
 public enum Query {
 
+    // Region Query
     GET_REGION("SELECT * FROM regions"),
     GET_BY_ID_REGION("SELECT * FROM regions WHERE region_id = ?"),
     UPDATE_REGION("UPDATE regions SET region_name = ? WHERE region_id = ?"),
     DELETE_REGION("DELETE FROM regions WHERE region_id = ?"),
     INSERT_REGION("INSERT INTO regions(region_name, region_id) VALUES (?, ?)"),
-    INSERT_OR_UPDATE_REGION("INSERT INTO regions(region_id, region_name) VALUES (?, ?) ON DUPLICATE KEY UPDATE region_name = ?");
+    
+    // Job Query
+    GET_JOB("SELECT * FROM jobs"),
+    GET_BY_ID_JOB("SELECT * FROM jobs WHERE job_id = ?"),
+    UPDATE_JOB("UPDATE jobs SET job_title = ?, min_salary = ?, max_salary = ? WHERE job_id = ?"),
+    DELETE_JOB("DELETE FROM jobs WHERE job_id = ?"),
+    INSERT_JOB("INSERT INTO jobs(job_title, min_salary, max_salary, job_id) VALUES (?, ?, ?, ?) ");
+    
 
     private final String displayQuery;
 
